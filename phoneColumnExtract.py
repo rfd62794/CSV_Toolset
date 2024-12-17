3dd28a89d1a7989dd2a82164f8e9b46f00e393c1
ConsumrBuzzy <robert.d@consumrbuzz.com>
2024-12-17 16:35:21 -0500

    Update CSV reading error handling in phone data extraction: Changed 'error_bad_lines' to 'on_bad_lines' in pd.read_csv for improved handling of malformed lines during CSV processing.

--- phoneColumnExtract.py
+++ phoneColumnExtract.py
@@ -29,7 +29,7 @@ def process_csv_files(source_path, destination_folder):
 
     for file in files:
         try:
-            df = pd.read_csv(file, low_memory=False, sep=',', quoting=csv.QUOTE_MINIMAL, error_bad_lines=False, warn_bad_lines=True)
+            df = pd.read_csv(file, low_memory=False, sep=',', quoting=csv.QUOTE_MINIMAL, on_bad_lines='warn')
             if 'Phone' in df.columns:
                 # Attempt to convert 'Phone' column to numeric, coercing errors to NaN
                 df['Phone'] = pd.to_numeric(df['Phone'], errors='coerce')
